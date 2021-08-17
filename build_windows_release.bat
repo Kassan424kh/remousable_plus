959420e5e015298602eead52420a6e4a96e36ee2
Khalil Khalil <k.khalil@satzmedia.de>
2021-08-17 12:33:14 +0200

    renamed add_image_to_exe_output.bat to build_windows_release.bat and updated the code inside it to build the project at first then add the icon to the exe file

--- build_windows_release.bat
+++ build_windows_release.bat
@@ -1,9 +1,6 @@
 # to convert the png logo to ico file just go to https://icoconvert.com/ website and upload the logo and select all options in the Custom sizes radio, at last download the ico file and move/copy it to go/assets/ folder
 
-@echo off 
+@echo off
 
-set root-dir = %cd%
-set release-outbut-dir = 
-set assets-outbut-dir = %release-outbut-dir%\assets
-
+hover build windows --release
 %cd%\rh.exe -open %cd%\go\build\outputs\windows-release\remousable_plus.exe -save %cd%\go\build\outputs\windows-release\remousable_plus.exe -action addskip -res %cd%\go\build\outputs\windows-release\assets\icon.ico -mask ICONGROUP,MAINICON,
